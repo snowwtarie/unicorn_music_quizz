b9bd04784c7674ac373b6c7425a29b902c4c9b99
miya <hamza.amalou@yahoo.fr>
2016-12-01 10:01:00 +0100

    MàJ pom.xml + nouvelle classe pour l'appel de l'API Deezer

--- src/com/imie/unicorn/controller/FrontController.java
+++ src/com/imie/unicorn/controller/FrontController.java
@@ -1,18 +1,20 @@
 package com.imie.unicorn.controller;
 
-import java.net.MalformedURLException;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class FrontController {
 
-    public static void main(String[] args) throws MalformedURLException, InterruptedException {
-        UnicornCore unicornCore = new UnicornCore();
-        unicornCore.addPlayer(new Player("192.168.1.1", "Hamza", "a", 0, true));
-        unicornCore.addPlayer(new Player("192.168.1.2", "Marius", "g", 0, true));
-        unicornCore.addPlayer(new Player("192.168.1.3", "Thibault", "r", 0, true));
-        unicornCore.addPlayer(new Player("192.168.1.4", "Mathieu", "p", 0, true));
-
-        Thread.sleep(4000);
-        unicornCore.checkProposition("Daft Punk", "Hamza192.168.1.1");
+    public static void main(String[] args) {
+        DeezerAPI deezerAPI = new DeezerAPI(1366766145);
+        try {
+            ArrayList<Track> a = deezerAPI.getListTrack();
+            for (Track track : a) {
+                System.out.println(track);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
---
+++ src/com/imie/unicorn/controller/Playlist.java
@@ -0,0 +1,19 @@
+package com.imie.unicorn.controller;
+
+import com.google.gson.JsonElement;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Playlist {
+
+}
+
+
+
+
+
+
+
+
