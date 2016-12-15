5b503811c07f36d864122eb9b9dcc6ef2f839642
yornletard <yornletard@gmail.com>
2016-12-15 12:05:19 +0100

    ServerCentral added method sendToAll2

--- src/main/java/com/imie/unicorn/model/ThreadServer.java
+++ src/main/java/com/imie/unicorn/model/ThreadServer.java
@@ -40,8 +40,10 @@ public class ThreadServer extends  Thread {
             actionServer.addPlayer((Player) message.getValue());
             this.sendMessage(new Message("Connexion", null));
         } else if (message.getKey().equals("List_Players")) {
-            actionServer.sendToAll(new Message("List_Players", actionServer.getListPlayers()));
+            actionServer.sendToAll(new Message("refreshListPlayer", actionServer.getListPlayers()));
             System.out.println("Thread >>> LISTPLAYER");
+        }else if (message.getKey().equals("refreshListPlayer")){
+            sendMessage(new Message("List_Player", message.getValue()));
 
         } else if (message.getKey().equals("PlayerReady")) {
             Player player = (Player) message.getValue();
