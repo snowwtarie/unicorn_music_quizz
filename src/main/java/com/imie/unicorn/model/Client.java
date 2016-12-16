c0e10f849274e7ab2d9a5ea9890bbd0b98c12f2d
yornletard <yornletard@gmail.com>
2016-12-16 12:16:16 +0100

    Fix

--- src/main/java/com/imie/unicorn/model/Client.java
+++ src/main/java/com/imie/unicorn/model/Client.java
@@ -33,7 +33,7 @@ public class Client {
 
     public Client() throws IOException {
         //Socket socketClient = new Socket("192.168.43.43", 5000);
-        Socket socketClient = new Socket("127.0.0.1", 5000);
+        Socket socketClient = new Socket("10.4.1.14", 5000);
 
         out = new ObjectOutputStream(socketClient.getOutputStream());
         out.flush();
